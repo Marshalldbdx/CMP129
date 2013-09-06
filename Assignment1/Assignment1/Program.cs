64286c8bca6815a85ba89e1252629d2e6ad094d9
Marshall <marshalldbdx@gmail.com>
2013-09-05 20:23:51 -0400

    hi

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            //Console.ForegroundColor = ConsoleColor.Red;
+         //   Console.WriteLine(int);
+         //   Console.WriteLine("another line.");
+           // Console.ResetColor();
+            String myString = x;
+            if (x = 0) 
+            {
+                x++;
+
+                while(x <= 100)
+                {
+                    x++;
+                }
+            }
+        }
+    }
+}
