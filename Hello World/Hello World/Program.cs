62ad5dbee0e7a09e4aae5114ee1aa99f0eaa425d
Marshall <marshalldbdx@gmail.com>
2013-09-05 20:21:08 -0400

    hi

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string myString = "Hello World From String";
+            char myChar = 'a';
+            int myInt = 5;
+            float myFloat = 5f;
+            double myDbl = 5;
+            bool myBool = false;
+
+            
+        }
+
+        private static void MyFunc;
+        
+    }
+}
